6ea114062c25911ff2366b2fad2d1e753cdf3919
Brandon Werner <brandon.werner@microsoft.com>
2015-04-16 11:45:37 -0700

    Added PEM file creation and ODIC template

--- node-server/config.js
+++ node-server/config.js
@@ -1,6 +1,5 @@
  // Don't commit this file to your public repos
     exports.creds = {
     mongoose_auth_local: 'mongodb://localhost/tasklist', // Your mongo auth uri goes here
-    secret_key: 'Ly9S28kcMRTueWRpZ4w89kTX5gbBb2He/pB52dx65UA=', // Your Secret Key
-    audience: '', // Audience URI
+    openid-configuration: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
 }
--- node-server/lib/jwt_strategy.js
+++ node-server/lib/jwt_strategy.js
@@ -1,71 +1,27 @@
+/*
+ Copyright (c) Microsoft Open Technologies, Inc.
+ All Rights Reserved
+ Apache License 2.0
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 "use strict";
 var BearerStrategy = require('passport-http-bearer').Strategy
   , jwt = require('jsonwebtoken')
   , util = require('util');
 
-/**
- * Creates an instance of `Strategy`.
- *
- * The HTTP JWT Bearer authentication strategy authenticates requests based on
- * a JWT encoded ans signed bearer token contained in the `Authorization`
- * header field, `access_token` body parameter, or `access_token` query
- * parameter.
- *
- * Application must supply either the secret for HMAC algorithms, or the PEM
- * encoded public key for RSA and ECDSA algorithms.
- *
- * Applications must supply a `verify` callback, for which the function
- * signature is:
- *
- *     function(token, done) { ... }
- *
- * `token` is the verified and decoded bearer token provided as a credential.
- * The verify callback is responsible for finding the user who posesses the
- * token, and invoking `done` with the following arguments:
- *
- *     done(err, user, info);
- *
- * If the token is not valid, `user` should be set to `false` to indicate an
- * authentication failure.  Additional token `info` can optionally be passed as
- * a third argument, which will be set by Passport at `req.authInfo`, where it
- * can be used by later middleware for access control.  This is typically used
- * to pass any scope associated with the token.
- *
- * Options:
- *
- *   - `realm`    authentication realm, defaults to "Users"
- *   - `scope`    list of scope values indicating the required scope of the
- *                access token for accessing the requested resource
- *   - `audience` if you want to check JWT audience (aud), provide a value here
- *   - `issuer`   if you want to check JWT issuer (iss), provide a value here
-
- *
- * Examples:
- *
- *     passport.use(new JwtBearerStrategy(
- *       secretOrPublicKey
- *       function(token, done) {
- *         User.findById(token.sub, function (err, user) {
- *           if (err) { return done(err); }
- *           if (!user) { return done(null, false); }
- *           return done(null, user, token);
- *         });
- *       }
- *     ));
- *
- * For further details on HTTP Bearer authentication, refer to [The OAuth 2.0 Authorization Protocol: Bearer Tokens](http://tools.ietf.org/html/draft-ietf-oauth-v2-bearer)
- * For further details on JSON Web Token, refert to [JSON Web Token](http://tools.ietf.org/html/draft-ietf-oauth-json-web-token)
- *
- * @param {string|Buffer} secretOrPublicKey - A string or buffer containing
- *   either the secret for HMAC algorithms, or the PEM encoded public key for
- *   RSA and ECDSA algorithms.
- * @param {object} options - The Options.
- * @param {Function} verify - The verify callback.
- * @constructor
- */
 function Strategy(secretOrPublicKey, options, verify) {
   if (!secretOrPublicKey || (typeof secretOrPublicKey != 'string' && !(secretOrPublicKey instanceof Buffer))) {
-    console.log(secretOrPublicKey);
     throw new TypeError('JwtBearerStrategy requires a string or buffer containing either the secret for HMAC algorithms, or the PEM encoded public key for RSA and ECDSA callback');
   }
   if (typeof options == 'function') {
@@ -84,7 +40,7 @@ function Strategy(secretOrPublicKey, options, verify) {
       done = arguments[1];
       req = null;
     }
-    jwt.verify(token, secretOrPublicKey, options, function (err, token) {
+    jwt.verify(token, secretOrPublicKey, {algorithms: ["RS256"]}, function (err, token) {
       if (err) {
         if (err instanceof jwt.TokenExpiredError) {
           done(null, false, 'The access token expired');
