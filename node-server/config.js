8287ca26fd538a3bc9ec68d0dc6cd81a3ff1cda3
Brandon Werner <brandon.werner@microsoft.com>
2015-06-02 14:30:40 -0700

    Update back to business scenarios

--- node-server/config.js
+++ node-server/config.js
@@ -2,5 +2,5 @@
     exports.creds = {
     mongoose_auth_local: 'mongodb://localhost/tasklist', // Your mongo auth uri goes here
     audience: 'http://localhost:8888/', // the Audience is the App URL when you registered the application.
-    identityMetadata: 'https://login.microsoftonline.com/hypercubeb2c.onmicrosoft.com/.well-known/openid-configuration?p=b2c_1_B2CSI' // Replace the text after p= with your specific policy.
+    identityMetadata: 'https://login.microsoftonline.com/common/' // Replace the text after p= with your specific policy.
   };
